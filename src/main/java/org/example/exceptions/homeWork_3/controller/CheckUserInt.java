5a0c2d5c78c3c762b0ad6c557264d2a4c5f5aef6
Julia <julia.masaeva@gmail.com>
2023-08-04 22:58:52 +0600

    ДЗ по 3 семинару

---
+++ src/main/java/org/example/exceptions/homeWork_3/controller/CheckUserInt.java
@@ -0,0 +1,11 @@
+package org.example.exceptions.homeWork_3.controller;
+
+public interface CheckUserInt {
+    String checkName(String name);
+
+    String checkDate(String date);
+
+    String checkNumber(String number);
+
+    String checkGender(String gender);
+}
